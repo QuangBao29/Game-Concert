3f605b23cd7e2a429f0026e8974d48e470e81248
QuangBao <bao.nguyen2969@gmail.com>
2024-05-12 16:22:42 +0700

    - hide materials in activator prefab

--- Assets/Scripts/Game/Gameplay/Activator.cs
+++ Assets/Scripts/Game/Gameplay/Activator.cs
@@ -24,7 +24,7 @@ public class Activator : MonoBehaviour
     private List<Note> notes = new();
     private List<NoteName> pitches = new();
     private int spawnIndex = 0;
-    private int inputIndex = 0;
+    private int noteIndex = 0;
     private int zoneIndex = 0;
     private float cooldown = Define.HitObjectInterval;
     private float lastClickedTime = 0;
@@ -74,79 +74,41 @@ public class Activator : MonoBehaviour
 
         if (Input.GetKeyDown(KeyInput) && Time.time - lastClickedTime > cooldown)
         {
-            OnClickHitButton();
             //check notes[0] xem thoi gian khi button dc click co nam trong khoang thoi gian cho phep hay khong
-            var note = notes[0];
-            double timeStamp = noteTimeMidi[0];
-            double marginOfError = SongManager.Instance.MarginOfError;
-            double audioTime = SongManager.GetAudioSourceTime() - (SongManager.Instance.InputDelayInMilliseconds / 1000.0);
-            if (Math.Abs(audioTime - timeStamp) < marginOfError)
+            if (notes.Count != 0)
             {
-                Debug.LogError("Hit on note");
-                OnNoteHit.Invoke(null, HitType.Perfect);
+                //add effect for button
+                lastClickedTime = Time.time;
+                var note = notes[0];
+                double timeStamp = noteTimeMidi[noteIndex];
+                double marginOfError = SongManager.Instance.MarginOfError;
+                double audioTime = SongManager.GetAudioSourceTime() - (SongManager.Instance.InputDelayInMilliseconds / 1000.0);
+                if (Math.Abs(audioTime - timeStamp) < marginOfError)
+                {
+                    //Debug.LogError("Hit on note");
+                    OnNoteHit.Invoke(null, HitType.Perfect);
+                }
+                else
+                {
+                    //Debug.LogError(String.Format("Hit inaccurate on note with {0} delay", audioTime - timeStamp));
+                    OnNoteHit.Invoke(null, HitType.Miss);
+                }
+                notes.RemoveAt(0);
+                note.OnFinishNote();
+                noteIndex++;
             }
-            else
-            {
-                Debug.LogError(String.Format("Hit inaccurate on note with {0} delay", Math.Abs(audioTime - timeStamp)));
-                OnNoteHit.Invoke(null, HitType.Miss);
-            }
-            var temp = notes[0];
-            notes.RemoveAt(0);
-            temp.OnFinishNote();
         }
-        //if (hitZone.transform.position != originalHitPos && isClicked)
-        //{
-        //    Invoke("OnClickUpKeyInput", Define.HitObjectInterval);
-        //}
-
-        //if (inputIndex < noteTimeMidi.Count)
-        //{
-        //    double timeStamp = noteTimeMidi[inputIndex];
-        //    double marginOfError = SongManager.Instance.MarginOfError;
-        //    double audioTime = SongManager.GetAudioSourceTime() - (SongManager.Instance.InputDelayInMilliseconds / 1000.0);
-
-        //    if (Input.GetKeyDown(KeyInput))
-        //    {
-        //        if (Math.Abs(audioTime - timeStamp) < marginOfError)
-        //        {
-        //            //Hit();
-        //            Debug.LogError(String.Format("Hit on {0} note", inputIndex + 1));
-        //            var temp = notes[inputIndex];
-        //            Destroy(temp.gameObject);
-        //            inputIndex++;
-        //        }
-        //        else
-        //        {
-        //            //Debug.LogError(String.Format("Hit inaccurate on {0} note with {1} delay", inputIndex, Math.Abs(audioTime - timeStamp)));
-        //            //Debug.LogError("tre");
-        //        }
-        //    }
-
-        //    if (timeStamp + marginOfError <= audioTime)
-        //    {
-        //        ScoreManager.Miss();
-        //        inputIndex++;
-        //    }
-        //}
-    }
-
-    private void OnClickHitButton()
-    {
-        lastClickedTime = Time.time;
     }
 
     public void OnResponseNoteMiss(Component component, object data)
     {
-        int index;
-        if (data is int)
+        if (notes.Count == 0)
         {
-            index = (int)data;
-            var temp = notes[index];
-            Destroy(temp.gameObject);
+            return;
         }
-        else
-        {
-            Debug.LogError("Wront data pack in OnResponseNoteMiss");
-        }
+        var note = notes[0];
+        notes.RemoveAt(0);
+        note.OnFinishNote();
+        noteIndex++;
     }
 }
